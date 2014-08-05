3072b19f807219dc1c515a87b24812d36e82146c
Robert Lord <rlord@stripe.com>
2014-08-05 13:31:20 -0700

    Add basic compiler

--- src/compile.js
+++ src/compile.js
@@ -1,4 +1,22 @@
-var compile = function(string) {
-  return string;
+var compile = function(ast) {
+  return (function(data) {
+    console.log("AST: %j", ast);
+    console.log("Data: %j", data);
+    var output = "";
+    for (var i in ast.tree) {
+      switch(ast.tree[i].type) {
+      case "block":
+        output += compile(ast.tree[i])(data);
+        break;
+      case "string":
+        output += ast.tree[i].val;
+        break;
+      case "var":
+        output += data[ast.tree[i].val];
+        break;
+      }
+    }
+    return output;
+  });
 };
 module.exports = compile;
